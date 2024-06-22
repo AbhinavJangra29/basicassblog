2743c3af581e6b65a618c4817926942971d7efa3
AbhinavJangra29 <abhinavjangra619@gmail.com>
2024-06-22 19:55:20 +0530

    navbar changed

--- basic-ass-blog/src/components/Navbar/Navbar.jsx
+++ basic-ass-blog/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css'; // Import the CSS file for styles
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>Abhinav's Blog</h1>
+            <h1>Abhinav ka Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create" className="new-blog">New Blog</a>
